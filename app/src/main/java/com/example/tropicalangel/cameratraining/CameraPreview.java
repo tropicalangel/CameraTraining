f585ec4c75723a831b46db20cd197dddd1bc3cfc
tropical-angel <kaihaku.p@gmail.com>
2017-06-03 22:10:03 +0900

    late first commit

---
+++ app/src/main/java/com/example/tropicalangel/cameratraining/CameraPreview.java
@@ -0,0 +1,68 @@
+package com.example.tropicalangel.cameratraining;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by tropicalangel on 2017/05/30.
+ */
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        mHolder = this.getHolder();
+        mHolder.addCallback(this);
+        Log.e("holder_2",mHolder.toString());
+
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            Log.e("camera",mCamera.toString());
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch(IOException e) {
+            Log.d("surface create","Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        if (mHolder.getSurface() == null){
+            return;
+        }
+
+        try {
+            mCamera.stopPreview();
+        } catch(Exception e) {
+
+        }
+
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+        } catch (Exception e){
+            Log.d("surface change","Error start camera preview: " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
---
+++ app/src/main/java/com/example/tropicalangel/cameratraining/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.tropicalangel.cameratraining;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Picture;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements Camera.ErrorCallback {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private static Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_preview);
+
+        mContext = this;
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA},1);
+        } else {
+            if (checkCameraHardWare(this)) {
+                mCamera = getCameraInstance();
+            }
+        }
+//        if (checkCameraHardWare(this)) {
+//            mCamera = getCameraInstance();
+//
+//            mPreview = new CameraPreview(this, mCamera);
+//        }
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout)findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        Button pictureButton = (Button) findViewById(R.id.button_capture);
+        pictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null,null,mPicture);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions,int[] grantResult){
+        switch(requestCode){
+            case 1:
+                if (grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkCameraHardWare(this)) {
+                        mCamera = getCameraInstance();
+
+                    }
+                }
+        }
+    }
+
+
+    private boolean checkCameraHardWare(Context context){
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            return true;
+        } else {
+            Log.e("check_Camera","do not");
+            return false;
+        }
+    }
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch(Exception e) {
+            Log.e("getCameraInstance","Error camera instance: " + e.getMessage());
+        }
+        return c;
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            Log.d("second media file","name: " + pictureFile.getName());
+            if (pictureFile == null){
+                Log.d("onPictureTaken","Error creating media file, check storage permissions: null?");
+                return;
+            }
+            try{
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch(FileNotFoundException e){
+                Log.d("onPictureTaken","File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d("onPictureTaken","Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        releaseMediaRecorder();
+        releaseCamera();
+    }
+
+    private void releaseMediaRecorder(){
+
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    private static File getOutputMediaFile(int type){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"MyCameraApp");
+        Log.e("outputMedia","dir: " + mediaStorageDir);
+        if (! mediaStorageDir.exists()){
+            if (ContextCompat.checkSelfPermission(mContext,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                Log.d("permission check","false");
+            } else {
+                boolean b = mediaStorageDir.mkdirs();
+                Log.e("boolean" , b + "");
+                if (! b){
+                    Log.e("MyCameraApp","failed to create directory");
+                    return null;
+                }
+            }
+        }
+        boolean b = mediaStorageDir.mkdirs();
+        File f = new File(String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)));
+        String[] files = f.list();
+        Log.d("Environment file",f.getPath() + " : " + b);
+        for (String file:files){
+            Log.d("file name",file);
+        }
+
+        String timeStamp = new SimpleDateFormat("yyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+        Log.d("first media file","name: " + mediaFile.getName());
+        return mediaFile;
+    }
+
+    @Override
+    public void onError(int error, Camera camera) {
+        Log.e("camera_error" , "Error camera onError" + error);
+    }
+}
